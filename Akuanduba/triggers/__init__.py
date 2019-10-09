2eb1b7aa8b41563c7095d522d46ae32e723220ed
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2019-10-09 10:55:50 -0300

    Remove legacy trigger conditions

--- Akuanduba/triggers/__init__.py
+++ Akuanduba/triggers/__init__.py
@@ -1,9 +1,5 @@
 __all__ = []
 
-from . import Clock
-__all__.extend(Clock.__all__)
-from .Clock import *
-
 from . import TimerCondition
 __all__.extend(TimerCondition.__all__)
 from .TimerCondition import *
