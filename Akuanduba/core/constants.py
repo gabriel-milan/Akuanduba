ed5eb4c5cc88411ea1349ad461800b5b736207b1
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2019-10-09 12:05:28 -0300

    Prettify constants

--- Akuanduba/core/constants.py
+++ Akuanduba/core/constants.py
@@ -1,16 +1,16 @@
 # Time constants
-Second = 1.
-Minute = 60 * Second
-Hour = 60 * Minute
-Day = 24 * Hour
-Week = 7 * Day
-Year = 365.25 * Day
+Second                          = 1.
+Minute                          = 60 * Second
+Hour                            = 60 * Minute
+Day                             = 24 * Hour
+Week                            = 7 * Day
+Year                            = 365.25 * Day
 
 # CoreClasses constants
-MAX_FIFO = 100000
+MAX_FIFO                        = 100000
 
 # StoreGateSvc constants
-relative_save_dir = 'saves/'
+relative_save_dir               = 'saves/'
 
 # Watchdog constants
 DEFAULT_METHOD_ENABLE           = True
